3f9fa96c69c4559e5074d8629f7108a289213760
casie <casie.levine@gmail.com>
2015-03-07 16:21:17 -0500

    delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,31 @@ class PostsController < ApplicationController
     erb :"search/edit_posts.html"
   end
 
+  post '/posts/:id/destroy' do
+   @post = Post.find(params[:id])
+   @post.destroy
+    locate_redirect(@post)
+  end
+
   patch "/posts/:id" do # UPDATE
     @post = Post.find(params[:id])
     @post.update(params[:post])
+      locate_redirect(@post)
+  end
 
-    if @post.podcast_id
-      redirect "/podcasts/#{@post.podcast_id}"
-    elsif @post.episode_id
-      episode_id = @post.episode_id
+  def locate_redirect(post)
+    if post.podcast_id
+      redirect "/podcasts/#{post.podcast_id}"
+    elsif post.episode_id
+      episode_id = post.episode_id
       episode = Episode.find(episode_id)
       podcast = episode.podcast
       redirect "/podcasts/#{podcast.id}/#{episode_id}" 
     end
   end
+
+
+end  
 
   # get '/posts' do # INDEX
   #   @posts = Post.all
@@ -58,7 +70,7 @@ class PostsController < ApplicationController
   # end  
 
 
-end
+
 
 # 1. Index all blogs posts
 # 2. show a blog posts
